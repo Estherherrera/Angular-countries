9b5e97fbaaf85f53d18dd2e2038ba791fd22f621
Esther Herrera <graxyherrera@gmail.com>
2024-08-12 12:48:06 +0200

    Mejoras, indicador de la carga, optimización de busqueda por region y otros más

--- src/app/countries/pages/by-coutry-page/by-coutry-page.component.ts
+++ src/app/countries/pages/by-coutry-page/by-coutry-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
@@ -7,11 +7,19 @@ import { CountriesService } from '../../services/countries.service';
   templateUrl: './by-coutry-page.component.html',
   styles: ``
 })
-export class ByCoutryPageComponent {
+export class ByCoutryPageComponent implements OnInit {
 
   public countries: Country[] = [];
+  public initialValue: string = '';
 
   constructor( private countriesService: CountriesService){}
+
+
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountries.countries
+    this.initialValue = this.countriesService.cacheStore.byCountries.term
+  }
 
   searchByCountry(term: string): void {
     this.countriesService.searchCountry(term)
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+
 import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
@@ -15,6 +17,7 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     AboutPageComponent,
     ContactPageComponent,
     HomePagesComponent,
+    LoadingSpinnerComponent,
     SearchBoxComponent,
     SidebarComponent,
 
@@ -27,6 +30,7 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     AboutPageComponent,
     ContactPageComponent,
     HomePagesComponent,
+    LoadingSpinnerComponent,
     SearchBoxComponent,
     SidebarComponent,
   ]
